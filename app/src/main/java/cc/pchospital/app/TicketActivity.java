e6f85c6bab6fbce6914f65ad37dc224af7724f62
Ike <ikesnowy@outlook.com>
2017-10-09 15:30:37 +0800

    Initial commit

---
+++ app/src/main/java/cc/pchospital/app/TicketActivity.java
@@ -0,0 +1,16 @@
+package cc.pchospital.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class TicketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ticket);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+    }
+}
